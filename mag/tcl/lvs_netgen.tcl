f1f654c11efdd3fd61cfcf33ce1bca8b88b604b0
Anton Maurovic <anton@foogle.com>
2025-08-30 23:44:13 +0930

    Fix LVS script: Support icellwrap<=>icell comparison

--- mag/tcl/lvs_netgen.tcl
+++ mag/tcl/lvs_netgen.tcl
@@ -1,18 +1,18 @@
-set layout [readnet spice $project.lvs.spice]
-set source [readnet spice /dev/null]
-readnet spice $::env(PDK_ROOT)/$::env(PDK)/libs.ref/sky130_fd_sc_hd/spice/sky130_fd_sc_hd.spice $source
+set layout_netlist_file_handle [readnet spice $layout_name.lvs.spice]
+set schem_netlist_file_handle  [readnet spice /dev/null]
+readnet spice $::env(PDK_ROOT)/$::env(PDK)/libs.ref/sky130_fd_sc_hd/spice/sky130_fd_sc_hd.spice $schem_netlist_file_handle
 
 #NOTE: For simplicity, ALL of the files below can be loaded for every
 # LVS run, because even if you're LVSing just one block, the last
 # 'lvs' command selects which one actually matters.
 
 # Add spice files of analog block(s):
-readnet spice ../xschem/simulation/icell.spice     $source
+readnet spice ../xschem/simulation/icell.spice     $schem_netlist_file_handle
 
 # # Add GL verilog of digital block(s) (i.e. flat file from OpenLane hardening):
-# readnet verilog ../verilog/gl/analog_control_wrapper.pnl.v $source
+# readnet verilog ../verilog/gl/analog_control_wrapper.pnl.v $schem_netlist_file_handle
 
 # # Top-level abstract integration verilog:
-# readnet verilog ../src/project.v $source
+# readnet verilog ../src/project.v $schem_netlist_file_handle
 
-lvs "$layout $project" "$source $project" $::env(PDK_ROOT)/$::env(PDK)/libs.tech/netgen/sky130A_setup.tcl lvs.report -blackbox
+lvs "$layout_netlist_file_handle $layout_name" "$schem_netlist_file_handle $schem_name" $::env(PDK_ROOT)/$::env(PDK)/libs.tech/netgen/sky130A_setup.tcl lvs.report -blackbox
